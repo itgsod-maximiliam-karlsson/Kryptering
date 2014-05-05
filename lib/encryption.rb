90fdd7de95f944d8af76da8c72de404bc0d9eef6
Maximiliam Karlsson <maximiliam.karlsson@elev.it-gymnasiet.se>
2014-05-05 14:37:53 +0200

    changed the position of the list_char.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,8 +1,8 @@
-list_char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty' if input.empty?
   raise ArgumentError, 'Offset must not be zero' if offset == 0
-
+  list_char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  
 end
 
 
