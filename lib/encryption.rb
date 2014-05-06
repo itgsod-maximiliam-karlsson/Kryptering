45634bf5c1bd4f28e6caeea0294663ca519a576f
Maximiliam Karlsson <maximiliam.karlsson@elev.it-gymnasiet.se>
2014-05-06 10:04:26 +0200

    Added some veribles.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,7 +2,9 @@ def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty' if input.empty?
   raise ArgumentError, 'Offset must not be zero' if offset == 0
   list_char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  
+  new_code = []
+  new_char = ''
+  list_code = input.split('')
 end
 
 
