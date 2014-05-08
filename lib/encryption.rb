cb0cb5c086ae9eec9a59084aa01b04a7a23db4a0
Maximiliam Karlsson <maximiliam.karlsson@elev.it-gymnasiet.se>
2014-05-08 10:44:59 +0200

    Fixed a Funktion to generate the reset value

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,21 +1,29 @@
+def reset_value(offset)
+  if offset > 0
+    25 - offset
+  end
+end
+
 def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty' if input.empty?
   raise ArgumentError, 'Offset must not be zero' if offset == 0
+
   list_char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   new_code = []
   new_char = ''
-  list_code = input.upcase.split(' ')
-  list_code.each do |word|
+  space = 32.chr
+  list_code = input.upcase.split('')
 
   list_code.each do |item|
     list_char.each_with_index do |number_char, index|
       if item == number_char
-        if index < 22
+        if index < reset_value(offset)
           new_code << index + offset
         else
-
           new_code << (index - 26) + offset
         end
+      elsif item == space
+        new_code << space
       end
     end
   end
@@ -24,11 +32,5 @@ def encrypt(input, offset)
     new_char = new_char + list_char[item]
   end
 
-
-  end
-
-  p new_char
+  new_char
 end
-
-
-
