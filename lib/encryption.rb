c834da83a624133f53b6fff1d8d7ef3e096d4378
Maximiliam Karlsson <maximiliam.karlsson@elev.it-gymnasiet.se>
2014-05-08 18:06:51 +0200

    Added a reset function that rests the pos

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,18 +1,43 @@
+def reset_value(pos, offset)
+  if offset > 0
+    value = pos + offset
+    if value <= 26
+      return value
+    else
+      while value > 26
+        value = value - 26
+      end
+      return value
+    end
+  else
+    value = pos + offset
+    if value >= 0
+      return value
+    else
+      while value < 0
+        value = value + 26
+      end
+      return value
+    end
+  end
+end
+
+
 def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty' if input.empty?
   raise ArgumentError, 'Offset must not be zero' if offset == 0
 
   alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  encrypted_input = []
+  encrypted_output = []
   input = input.upcase.split('')
 
   input.each do |char|
     pos = alphabet.index(char)
-    char_whih_offset = alphabet[pos + offset]
-    encrypted_input << char_whih_offset
+    char_whih_offset = alphabet[reset_value(pos, offset)]
+    encrypted_output << char_whih_offset
   end
 
-  encrypted_input.to_s
+  return encrypted_output.to_s
 end
 
 
@@ -21,14 +46,16 @@ def decrypt(input, offset)
   raise ArgumentError, 'Offset must not be zero' if offset == 0
 
   alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  decrypted_input = []
+  decrypted_output = []
   input = input.upcase.split('')
 
   input.each do |char|
     pos = alphabet.index(char)
-    char_whih_offset = alphabet[pos - offset]
-    decrypted_input << char_whih_offset
+    char_whih_offset = alphabet[reset_value(pos, offset)]
+    decrypted_output << char_whih_offset
   end
 
-  decrypted_input.to_s
+  return decrypted_output.to_s
 end
+
+
