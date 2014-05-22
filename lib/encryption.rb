2ec630b065b76b1e7d96343aae78b0cd8171acac
Maximiliam Karlsson <maximiliam.karlsson@elev.it-gymnasiet.se>
2014-05-22 10:37:55 +0200

    Fixed the space problem.
    Now the encryption can encrypt with spaces

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -27,35 +27,40 @@ def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty' if input.empty?
   raise ArgumentError, 'Offset must not be zero' if offset == 0
 
-  alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', ' ']
   encrypted_output = []
   input = input.upcase.split('')
 
   input.each do |char|
     pos = alphabet.index(char)
     char_whih_offset = alphabet[reset_value(pos, offset)]
-    encrypted_output << char_whih_offset
+    if char != 32.chr
+      encrypted_output << char_whih_offset
+    else
+      encrypted_output << 32.chr
+    end
   end
 
   return encrypted_output.to_s
 end
 
+p encrypt('test test', +20)
 
-def decrypt(input, offset)
-  raise ArgumentError, 'String must not be empty' if input.empty?
-  raise ArgumentError, 'Offset must not be zero' if offset == 0
-
-  alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  decrypted_output = []
-  input = input.upcase.split('')
-
-  input.each do |char|
-    pos = alphabet.index(char)
-    char_whih_offset = alphabet[reset_value(pos, offset)]
-    decrypted_output << char_whih_offset
-  end
-
-  return decrypted_output.to_s
-end
+# def decrypt(input, offset)
+#   raise ArgumentError, 'String must not be empty' if input.empty?
+#   raise ArgumentError, 'Offset must not be zero' if offset == 0
+#
+#   alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+#   decrypted_output = []
+#   input = input.upcase.split('')
+#
+#   input.each do |char|
+#     pos = alphabet.index(char)
+#     char_whih_offset = alphabet[reset_value(pos, offset)]
+#     decrypted_output << char_whih_offset
+#   end
+#
+#   return decrypted_output.to_s
+# end
 
 
